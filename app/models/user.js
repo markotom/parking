b995a76901505f93f3aab55a1b0bf45fbddec5d6
Marco Godínez <markotom@gmail.com>
2013-01-23 14:05:04 -0600

    Set fake user model

---
+++ app/models/user.js
@@ -0,0 +1,27 @@
+define(function() {
+
+  var users = [
+      { id: 1, username: 'admin',         password: 'palindromo11', email: 'mgodinez@filos.unam.mx' }
+    , { id: 2, username: 'academica',     password: 'palindromo11', email: 'sacadfyl@gmail.com' }
+    , { id: 3, username: 'profesionales', password: 'kMpZOKgs',     email: 'floresfarfanleticia@gmail.com' }
+    , { id: 4, username: 'posgrado',      password: '2u5XO1rg',     email: 'eramirez@unam.mx' }
+    , { id: 5, username: 'personal',      password: 'K201zwVH',     email: 'gonyuj@hotmail.com' }
+    , { id: 6, username: 'delefyl',       password: 'MQEsFLqz',     email: 'delefyl@yahoo.com.mx' }
+    , { id: 7, username: 'suayed',        password: 'wJqDVvYq',     email: 'divisionsuayedfilos@yahoo.com.mx' }
+    
+    , { id: 8,  username: 'biblio',       password: 'TTKeVAH5',     email: 'biblio.ffyl@gmail.com' }
+    , { id: 9,  username: 'dgi',          password: '9dkfc49n',     email: 'interculturales.unam@gmail.com' }
+    , { id: 10, username: 'latam',        password: '4kvpvZ5q',     email: 'latinoamericanos@posgrado.unam.mx' }
+    , { id: 11, username: 'filosofia',    password: 'eTNObd7c',     email: 'colefilos@gmail.com' }
+    , { id: 12, username: 'geografia',    password: '4H0ysSvz',     email: 'geografiafilos@gmail.com' }
+    , { id: 13, username: 'historia',     password: 'eVOv2EGr',     email: 'cliofyl@gmail.com' }
+    , { id: 14, username: 'letrasc',      password: '4VxrBVJd',     email: 'patvillase@yahoo.com.mx' }
+    , { id: 15, username: 'letrash',      password: 'DDcLEw5W',     email: 'colegiodehispanicas@gmail.com' }
+    , { id: 16, username: 'letrasm',      password: 'DeQ3eUF9',     email: 'noemi.novell@gmail.com' }
+    , { id: 17, username: 'teatro',       password: 'H4G9UGsk',     email: 'colegio.de.teatro.unam@gmail.com' }
+    , { id: 18, username: 'pedagogia',    password: 'UKz7oKy6',     email: 'colegiopedagogia@gmail.com' }
+  ];
+
+  return users;
+
+});
