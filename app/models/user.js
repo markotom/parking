82dd4cd65780c7cfc5ec9c341950d15188f606c0
Marco Godínez <markotom@gmail.com>
2013-06-11 11:31:48 -0500

    Added new passwords

--- app/models/user.js
+++ app/models/user.js
@@ -3,23 +3,23 @@ define(function() {
   var users = [
       { id: 1, username: 'admin',         password: 'palindromo11', email: 'mgodinez@filos.unam.mx' }
     , { id: 2, username: 'academica',     password: 'palindromo11', email: 'sacadfyl@gmail.com' }
-    , { id: 3, username: 'profesionales', password: 'palindromo11',     email: 'floresfarfanleticia@gmail.com' }
-    , { id: 4, username: 'posgrado',      password: 'palindromo11',     email: 'eramirez@unam.mx' }
-    , { id: 5, username: 'personal',      password: 'amZk',     email: 'gonyuj@hotmail.com' }
-    , { id: 6, username: 'delefyl',       password: 'palindromo11',     email: 'delefyl@yahoo.com.mx' }
-    , { id: 7, username: 'suayed',        password: 'palindromo11',     email: 'divisionsuayedfilos@yahoo.com.mx' }
+    , { id: 3, username: 'profesionales', password: '8nk7tr',     email: 'floresfarfanleticia@gmail.com' }
+    , { id: 4, username: 'posgrado',      password: 'eMeOQW',     email: 'eramirez@unam.mx' }
+    , { id: 5, username: 'personal',      password: 'amZkd1',     email: 'gonyuj@hotmail.com' }
+    , { id: 6, username: 'delefyl',       password: 'ad3bvg',     email: 'delefyl@yahoo.com.mx' }
+    , { id: 7, username: 'suayed',        password: 'LFcyER',     email: 'divisionsuayedfilos@yahoo.com.mx' }
     
-    , { id: 8,  username: 'biblio',       password: 'palindromo11',     email: 'biblio.ffyl@gmail.com' }
-    , { id: 9,  username: 'dgi',          password: 'palindromo11',     email: 'interculturales.unam@gmail.com' }
-    , { id: 10, username: 'latam',        password: 'palindromo11',     email: 'latinoamericanos@posgrado.unam.mx' }
-    , { id: 11, username: 'filosofia',    password: 'palindromo11',     email: 'colefilos@gmail.com' }
-    , { id: 12, username: 'geografia',    password: 'palindromo11',     email: 'geografiafilos@gmail.com' }
-    , { id: 13, username: 'historia',     password: 'palindromo11',     email: 'cliofyl@gmail.com' }
-    , { id: 14, username: 'letrasc',      password: 'palindromo11',     email: 'patvillase@yahoo.com.mx' }
-    , { id: 15, username: 'letrash',      password: 'palindromo11',     email: 'colegiodehispanicas@gmail.com' }
-    , { id: 16, username: 'letrasm',      password: 'palindromo11',     email: 'noemi.novell@gmail.com' }
-    , { id: 17, username: 'teatro',       password: 'RZ85',     email: 'colegio.de.teatro.unam@gmail.com' }
-    , { id: 18, username: 'pedagogia',    password: '8SqV',     email: 'colegiopedagogia@gmail.com' }
+    , { id: 8,  username: 'biblio',       password: 'q4P8Qo',     email: 'biblio.ffyl@gmail.com' }
+    , { id: 9,  username: 'dgi',          password: 'vRe4gg',     email: 'interculturales.unam@gmail.com' }
+    , { id: 10, username: 'latam',        password: 'Z1VrJF',     email: 'latinoamericanos@posgrado.unam.mx' }
+    , { id: 11, username: 'filosofia',    password: 'rGOdNL',     email: 'colefilos@gmail.com' }
+    , { id: 12, username: 'geografia',    password: 'Eo6QF8',     email: 'geografiafilos@gmail.com' }
+    , { id: 13, username: 'historia',     password: 'edHS4E',     email: 'cliofyl@gmail.com' }
+    , { id: 14, username: 'letrasc',      password: 'xGkNdf',     email: 'patvillase@yahoo.com.mx' }
+    , { id: 15, username: 'letrash',      password: 'MhoVJR',     email: 'colegiodehispanicas@gmail.com' }
+    , { id: 16, username: 'letrasm',      password: '5ytErz',     email: 'noemi.novell@gmail.com' }
+    , { id: 17, username: 'teatro',       password: 'RZ85d1',     email: 'colegio.de.teatro.unam@gmail.com' }
+    , { id: 18, username: 'pedagogia',    password: '8SqVd1',     email: 'colegiopedagogia@gmail.com' }
   ];
 
   return users;
